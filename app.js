6fc64687523e990ed356f4dd24bce46a0741a08c
bart7711 <bart0594@stud.kea.dk>
2022-02-20 17:02:08 +0100

    Added basic functionality

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get("/",(req,res) =>{
     res.sendFile(__dirname+"/public/main.html")
-})
+});
+
+app.get("/timer",(req,res)=>{
+    res.sendFile(__dirname+"/public/timer.html")
+});
 
 app.listen(8080, () =>{
     console.log("The countdown app has started.");
